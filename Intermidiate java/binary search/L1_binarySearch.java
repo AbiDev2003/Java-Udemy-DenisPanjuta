e5c9ab703980cadbd7a340cc07f335601baf1a15
AbiDev2003 <2003abinashdash@gmail.com>
2025-07-25 23:14:37 +0530

    Binary search algorithm is covered

---
+++ Intermidiate java/binary search/L1_binarySearch.java
@@ -0,0 +1,36 @@
+import java.util.Scanner; 
+
+public class L1_binarySearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        // first we gotta sort the array
+        int[] sortedArray = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        int target = 7; 
+
+        int result = binarySearch(sortedArray, target);
+        if(result == -1){
+            System.out.println("Element not found !");
+        }else{
+            System.out.println("The target is at index: " + result);
+        }
+        scanner.close(); 
+    }
+    public static int binarySearch(int[] sortedArray, int target){
+        int low = 0; 
+        int high = sortedArray.length - 1; 
+        while(low <= high){
+            int mid = low + (high - low) / 2; 
+            if(sortedArray[mid] == target){
+                return mid; 
+            }
+            if(sortedArray[mid] < target){
+                // target is the the last half
+                low = mid + 1;
+            }else {
+                high = mid - 1; 
+            }
+        }
+        return -1; 
+
+    }
+}
---
+++ Intermidiate java/binary search/L2_numberGuessGame.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class L2_numberGuessGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Guess a number between 1 to 100 and keep it in your mind. : ");
+        System.out.println("i will try to build according to yor feedback. "); 
+        numberGuess(scanner); 
+        scanner.close();
+    }  
+    public static void numberGuess(Scanner scanner){
+        int low = 1; 
+        int high = 100; 
+        boolean correct = false; 
+        while(!correct){
+            int guess = low + (high - low) / 2; 
+            System.out.println("Is the actual number is " + guess + " ?");
+            System.out.println("enter 'h' if the number is higher. ");
+            System.out.println("enter 'l' if the number is lower. ");
+            System.out.println("enter 'c' if the number is same. ");
+
+            char feedback = scanner.next().charAt(0); 
+            if(feedback == 'h'){
+                low = guess + 1; 
+            }else if(feedback == 'l'){
+                high = guess - 1; 
+            }else if(feedback == 'c'){
+                correct = true; 
+                System.out.println("I guessed your number !. It is " + guess);
+            }else{
+                System.out.println("Please enter only h, l or c. ");
+            }
+        }
+    }
+}
