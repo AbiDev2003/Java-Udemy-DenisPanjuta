1325b6f3c8ebdb11b40dc51ff1e0dcc3f8b34bb8
Abinash Dash <2003abinashdash@gmail.com>
2025-08-01 19:48:47 +0530

    Car.java file deleted.

--- Intermidiate java/OOP/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car {
-    String color; 
-    String model; 
-    int year; 
-
-    void showDetails(){
-        System.out.println("This car is a " + model + " and is " + color + " and was built in " + year);
-    }
-}
